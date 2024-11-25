332f5b5eb218dedcd867f6c1f96cdcf0e96f176d
coverfrog <justfroglab@gmail.com>
2024-11-25 23:32:06 +0900

    Input Update

--- Assets/Content/Rpg/Scripts/BirdBehaviour.cs
+++ Assets/Content/Rpg/Scripts/BirdBehaviour.cs
@@ -7,20 +7,6 @@ namespace Cf
     [RequireComponent(typeof(Rigidbody))]
     public class BirdBehaviour : MonoBehaviour
     {
-        [Title("Action")]
-        [SerializeField] private MoveAction moveAction;
-
-        private Rigidbody _rBody;
-        private Vector3? _moveEndPoint;
-
-        private void Awake()
-        {
-            _rBody = GetComponent<Rigidbody>();
-        }
-
-        private void Update()
-        {
-            moveAction.ToUpdate(ref _rBody, ref _moveEndPoint, 1.0f);
-        }
+ 
     }
 }
--- Assets/Core/Cf/Utils/Runtime/UtilCam.cs
+++ Assets/Core/Cf/Utils/Runtime/UtilCam.cs
@@ -6,29 +6,7 @@ namespace Cf
     {
         public static class Cam
         {
-            private static Camera MainCam { get; set; }
-
-            public static void SetMainCam(Camera cam)
-            {
-                MainCam = cam;
-            }
-
-            private static bool MainCamExist()
-            {
-                if (MainCam == null)
-                {
-                    MainCam = Camera.main;
-
-                    if (MainCam == null)
-                    {
-                        return false;
-                    }
-                }
-
-                return true;
-            }
-
-            public static void RayScreen()
+            public static void RayScreen(Camera cam)
             {
                 
             }
