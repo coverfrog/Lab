8fbfe82060afbedf6627b361e1920be2bb722b6a
ChangSik Kim <justfroglab@gmail.com>
2025-03-11 17:59:26 +0900

    Editor 에서 Enum 변환 형식을 Txt 파일로 미리 보여줌
    바로 cs 파일 작성 시 에러가 발생 가능성 있다.
    
    Dictonary 통해 서로 연결 해주는 구조
    
    Input Event Name → String → Input Action

--- Assets/Scripts/Input/InputManager.cs
+++ Assets/Scripts/Input/InputManager.cs
@@ -10,30 +10,35 @@ public class InputManager : MonoBehaviour
     
     private PlayerInput _mPlayerInput;
     private Dictionary<string, InputAction> _mInputActionDict;
+    private Dictionary<InputEventName, string> _mInputEventNameDict;
     
     private void Awake()
     {
         InputActionAsset inputActionAsset = Resources.Load<InputActionAsset>(InputActionAssetResourcesPath);
 
+        Debug.Assert(inputActionAsset, "Load Fail");
         if (!inputActionAsset)
         {
-            Debug.LogError("Load Fail");
             return;
         }
 
-        if (!_mPlayerInput)
-        {
-            _mPlayerInput = gameObject.GetComponent<PlayerInput>();
-        }
-
-        _mPlayerInput.actions = inputActionAsset;
-
+        GetComponents();
         GetInputActionDict(ref inputActionAsset, out _mInputActionDict);
+        GetInputEventNameDict(ref _mInputActionDict, out _mInputEventNameDict);
+        
+        _mPlayerInput.actions = inputActionAsset;
+    }
+
+    private void GetComponents()
+    {
+        if (!_mPlayerInput) _mPlayerInput = gameObject.GetComponent<PlayerInput>();
     }
 
     public static void GetInputActionDict(ref InputActionAsset inputActionAsset, out Dictionary<string, InputAction> inputActionDict)
     {
         inputActionDict = new Dictionary<string, InputAction>();
+
+        int missingCount = 0;
         
         foreach (InputActionMap inputActionMap in inputActionAsset.actionMaps)
         {
@@ -43,14 +48,40 @@ public class InputManager : MonoBehaviour
                 {
                     continue;   
                 }
+
+                ++missingCount;
             }
         }
+
+        Debug.Assert(missingCount <= 0,$"missing count : {missingCount}");
 
         if (inputActionDict.Count > 0)
         {
             return;
         }
-        
+
+        inputActionDict.Clear();
         inputActionDict.Add("Error", null);
+    }
+
+    private void GetInputEventNameDict(ref Dictionary<string, InputAction> inputActionDict, out Dictionary<InputEventName, string> inputEventNameDict)
+    {
+        inputEventNameDict = new Dictionary<InputEventName, string>();
+
+        int missingCount = 0;
+        
+        foreach (KeyValuePair<string, InputAction> pair in inputActionDict)
+        {
+            if (Enum.TryParse(pair.Key, false, out InputEventName eventName))
+            {
+                inputEventNameDict.TryAdd(eventName, pair.Key);
+                
+                continue;
+            }
+
+            ++missingCount;
+        }
+
+        Debug.Assert( missingCount <= 0 ,$"missing count : {missingCount}");
     }
 }
